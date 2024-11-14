2eb525e79803661d7674e9b62798e77f450191a5
Toon Van de Maele <toonvandemaele@gmail.com>
2024-11-14 11:11:53 +0100

    add depth anything estimator & updated habitat data iterator

--- vbgs/data/depth.py
+++ vbgs/data/depth.py
@@ -2,12 +2,19 @@ from PIL import Image
 import numpy as np
 import torch
 
+from pathlib import Path
+
+import sys
+import vbgs
+
 
 def load_depth_model(model_name, device):
     if model_name == "zoe":
         return load_zoe(device)
-    else:
+    elif model_name == "dpt":
         return load_dpt(device)
+    else:
+        return load_dav2(device)
 
 
 def load_dpt(device):
@@ -28,7 +35,14 @@ def load_dpt(device):
     def preprocess(x):
         return depth_transforms(x).to(device)
 
-    def post_process(x):
+    def post_process(x, shape):
+        x = torch.nn.functional.interpolate(
+            x.unsqueeze(1),
+            size=shape[:2],
+            mode="bicubic",
+            align_corners=False,
+        )
+
         scale = 0.000305
         shift = 0.1378
 
@@ -49,7 +63,70 @@ def load_zoe(device):
             Image.fromarray(img.astype(np.uint8)), output_type="tensor"
         ).unsqueeze(0)
 
-    return model, lambda x: x, lambda x: x
+    def post_process(x, shape):
+        x = torch.nn.functional.interpolate(
+            x.unsqueeze(1),
+            size=shape[:2],
+            mode="bicubic",
+            align_corners=False,
+        ).squeeze()
+        return x.detach().numpy()
+
+    return model, lambda x: x, post_process
+
+
+def load_dav2(device):
+    root_path = Path(vbgs.__file__).parent.parent
+    da_path = root_path / "../Depth-Anything-V2"
+    sys.path.append(str((da_path / "metric_depth").absolute()))
+
+    from depth_anything_v2.dpt import DepthAnythingV2
+
+    model_configs = {
+        "vits": {
+            "encoder": "vits",
+            "features": 64,
+            "out_channels": [48, 96, 192, 384],
+        },
+        "vitb": {
+            "encoder": "vitb",
+            "features": 128,
+            "out_channels": [96, 192, 384, 768],
+        },
+        "vitl": {
+            "encoder": "vitl",
+            "features": 256,
+            "out_channels": [256, 512, 1024, 1024],
+        },
+    }
+
+    encoder = "vitl"  # or 'vits', 'vitb'
+    # 'hypersim' for indoor model, 'vkitti' for outdoor model
+    dataset = "hypersim"
+    # 20 for indoor model, 80 for outdoor model
+    max_depth = 20 if dataset == "hypersim" else 80
+
+    model = DepthAnythingV2(
+        **{**model_configs[encoder], "max_depth": max_depth}
+    )
+    model.load_state_dict(
+        torch.load(
+            da_path
+            / f"checkpoints/depth_anything_v2_metric_{dataset}_{encoder}.pth",
+            map_location="cpu",
+        )
+    )
+
+    model = model.to(device).eval()
+
+    def preprocess(x):
+        # model takes BGR
+        return x[..., [2, 0, 1]]
+
+    def postprocess(x, shape):
+        return x
+
+    return model.infer_image, preprocess, postprocess
 
 
 def predict_depth(rgb, depth_model, preprocess, postprocess):
@@ -58,13 +135,6 @@ def predict_depth(rgb, depth_model, preprocess, postprocess):
     with torch.no_grad():
         prediction = depth_model(x)
 
-        prediction = torch.nn.functional.interpolate(
-            prediction.unsqueeze(1),
-            size=rgb.shape[:2],
-            mode="bicubic",
-            align_corners=False,
-        ).squeeze()
-
-    depth = postprocess(prediction)
+    depth = postprocess(prediction, x.shape)
 
     return depth
