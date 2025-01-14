185cca2521b6c7f1f518d6362e0aba7004d90743
Ozan Catal <ozan.catal@verses.ai>
2025-01-14 13:26:36 +0100

    got the update loop working

--- scripts/train_objects.py
+++ scripts/train_objects.py
@@ -30,6 +30,7 @@ import jax.random as jr
 import jax.numpy as jnp
 import optax
 
+import matplotlib.pyplot as plt
 from PIL import Image
 
 import vbgs
@@ -43,6 +44,22 @@ from vbgs.render.volume import render_gsplat
 from model_volume import get_volume_delta_mixture
 
 
+def batched(dataloader, bs=64):
+    i = 0
+    cams, xs = [], []
+    while i < len(dataloader):
+        for _ in range(bs):
+            if i >= len(dataloader):
+                return jnp.stack(xs), jnp.stack(cams)
+            cams.append(dataloader.load_camera_params(i)[0])
+            xs.append(dataloader.get_camera_frame(i)[0])
+            i += 1
+        cams = jnp.stack(cams)
+        xs = jnp.stack(xs)
+        yield xs, cams
+        cams, xs = [], []
+
+
 def finetune(model: DeltaMixture, data_params, data_path, subsample, n_iters, key):
     data_iter = BlenderDataIterator(
         data_path, data_params=data_params, subsample=subsample
@@ -50,42 +67,42 @@ def finetune(model: DeltaMixture, data_params, data_path, subsample, n_iters, ke
 
     key, subkey = jr.split(key)
     mu, si, alpha = model.extract_model(data_params)
-    optimizer = optax.adam(1e-4)
+    optimizer = optax.adam(1e-3)
     opt_state = optimizer.init((mu, si, alpha))
     c = int(data_iter.intrinsics[0, 2]), int(data_iter.intrinsics[1, 2])
     f = float(data_iter.intrinsics[0, 0]), float(data_iter.intrinsics[1, 1])
 
     def render(m, s, a, cam):
-        return render_gsplat(m, s, a, cam, c, f, data_iter.h, data_iter.w, jnp.ones(3))
+        return render_gsplat(m, s, a, cam, c, f, data_iter.h, data_iter.w, jnp.zeros(3))
 
     def loss(m, s, a, x, cam):
         x_hat = jax.lax.map(lambda c: render(m, s, a, c), cam)
-        return jnp.sqrt((x[..., :3] - x_hat) ** 2).mean()
+        x = jnp.reshape(x[..., :3], (x.shape[0], -1))
+        x_hat = jnp.reshape(x_hat, (x_hat.shape[0], -1))
+        return jnp.sqrt((x - x_hat) ** 2).mean(axis=-1).sum()
 
-    def batched(dataloader, bs=64):
-        i = 0
-        cams, xs = [], []
-        while i < len(dataloader):
-            for _ in range(bs):
-                if i >= len(dataloader):
-                    return jnp.stack(xs), jnp.stack(cams)
-                cams.append(dataloader.load_camera_params(i)[1])
-                xs.append(dataloader.get_camera_frame(i)[0])
-                i += 1
-            cams = jnp.stack(cams)
-            xs = jnp.stack(xs)
-            yield xs, cams
-            cams, xs = [], []
-
-    gradfn = jax.grad(loss, [0, 1, 2])
+    gradfn = jax.value_and_grad(loss, [0, 1, 2])
     for i in range(n_iters):
         batchloader = batched(
-            BlenderDataIterator(data_path, data_params=data_params, subsample=subsample)
+            BlenderDataIterator(
+                data_path, data_params=data_params, subsample=subsample
+            ),
+            bs=64,
         )
         for xs, cams in batchloader:
-            grads = tuple(gradfn(mu, si, alpha, xs, cams))
+            loss, grads = tuple(gradfn(mu, si, alpha, xs, cams))
             updates, opt_state = optimizer.update(grads, opt_state)
             mu, si, alpha = optax.apply_updates((mu, si, alpha), updates)
+        img_hat = render(mu, si, alpha, cams[0])
+        img_hat = img_hat / jnp.max(img_hat)  # Sometimes the render isn't normalized...
+        img = xs[0][..., :3] / 255
+        fig, axes = plt.subplots(1, 2, figsize=(4, 2))
+        axes[0].imshow(img)
+        axes[1].imshow(img_hat)
+        [a.axis("off") for a in axes.flatten()]
+        plt.savefig(f"output_{i}.png")
+        plt.close()
+        print(loss)
 
 
 def fit_continual(
@@ -152,7 +169,7 @@ def fit_continual(
 
     model = copy.deepcopy(prior_model)
 
-    data_iter._frames = data_iter._frames[::40]
+    # data_iter._frames = data_iter._frames[::40]
     metrics = dict({})
     prior_stats, space_stats, color_stats = None, None, None
     for step, x in tqdm(enumerate(data_iter), total=len(data_iter)):
@@ -182,7 +199,7 @@ def run_experiment(key, data_path, n_components, subsample, init_random, batch_s
         init_random=init_random,
         batch_size=batch_size,
     )
-    finetune(model, data_params, data_path, subsample, 10, key)
+    finetune(model, data_params, data_path, subsample, 100, key)
     return {}
 
 
